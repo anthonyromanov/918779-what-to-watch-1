e8acd4074388c26d65dfdc4dd7a22b6ae68eabe1
Anton Romanov <609990@gmail.com>
2022-06-30 23:50:37 +0300

    Добавляет переменную окружения

--- app/test.php
+++ app/test.php
@@ -12,10 +12,13 @@ use whatwatch\MovieFinder;
 
 require_once '../vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 $logger = new Logger('omdb');
 $logger->pushHandler(new StreamHandler('file.log'));
 
-$movies= new OmdbRepository($logger, new Client(['base_uri' => 'http://www.omdbapi.com', 'http_errors' => false]), '579fed43');
+$movies= new OmdbRepository($logger, new Client(['base_uri' => 'http://www.omdbapi.com', 'http_errors' => false]), $_ENV['APIKEY']);
 
 $finder = new MovieFinder($movies);
 $movie = $finder->find('tt3896198');
